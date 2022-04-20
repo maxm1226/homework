1e59fb5e52507ee475d3b3a69240498ec669ede1
wda22a08 <domou791226@gmail.com>
2022-04-20 13:02:28 +0800

    uptate ex2

--- homework/ex2.php
+++ homework/ex2.php
@@ -9,9 +9,53 @@
 </head>
 
 <body>
-    <?php 
-    
-    ?>
+<?php
+function num2cht($num){
+    $numc ="零,壹,貳,參,肆,伍,陸,柒,捌,玖";
+    $unic = ",拾,佰,仟";
+    $unic1 = "元整,萬,億,兆,京";
+    $numc_arr = explode(",", $numc);
+    $unic_arr = explode(",", $unic);
+    $unic1_arr = explode(",", $unic1);
+    $i = str_replace(",", "", $num);
+    $c0 = 0;
+    $str = array();
+    do{
+        $aa = 0;
+        $c1 = 0;
+        $s = "";
+        $lan = (strlen($i) >= 4) ? 4 : strlen($i);
+        $j = substr($i, -$lan);
+        while($j > 0){
+            $k = $j % 10;
+            if($k > 0) {
+                $aa = 1;
+                $s = $numc_arr[$k].$unic_arr[$c1].$s;
+            }elseif($k == 0) {
+                if($aa == 1) $s = "0".$s;
+            }
+            $j = intval($j / 10);
+            $c1 += 1;
+        }
+        $str[$c0] = ($s == '') ? '' : $s.$unic1_arr[$c0];
+        $count_len = strlen($i) - 4;
+        $i = ($count_len > 0) ? substr($i, 0, $count_len) : '';
+        $c0 += 1;
+    }while($i != '');
+    $string="";
+    foreach($str as $v) $string .= array_pop($str);
+    $string = preg_replace('/0+/', '零', $string);
+    return $string;
+}
+?>
+<?php
+$a = 123456789;
+$b = 123456;
+$c = 1500;
+echo $a." = ".num2cht($a)."\n";
+echo $b." = ".num2cht($b)."\n";
+echo $c." = ".num2cht($c)."\n";
+?>
 </body>
 
 </html>
