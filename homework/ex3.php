59895a07a35029a979fda6cca2f2b1a461befbdd
sin07222 <sinritter@gmail.com>
2022-04-23 15:17:23 +0800

    修改index中的ex3超連結與css樣式-睦庭

--- homework/ex3.php
+++ homework/ex3.php
@@ -1,17 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>尋找質數php程式</title>
+    <link rel="stylesheet" href="ex3.css">
+    <title>尋找質數PHP程式</title>
 </head>
-
 <body>
     <?php
-        
+    if(isset($_POST['numder01'])){
+        $default_val=$_POST['numder01'];
+    }else{
+        $default_val="";
+    }
+    ?>
+    <form action="ex3.php" method="post" name="form1" id="form1">
+        <h1 class="t1">尋找質數PHP程式</h1>
+        <p class="t2"><label>請輸入要尋找多少個質數:<br><input type="text" name="numder01" value="<?php echo $default_val;?>" id="numder01"></label></p>
+        <input type="hidden" name="MM_form" id="MM_form" value="form1">
+        <p><input type="submit" name="button" id="button" value="開始找尋"></p>
+    </form>
+    <?php
+    ini_set("error_reporting","E_ALL & ~E_NOTICE");
+    if(isset($_POST['MM_form']) and $_POST['MM_form']=='form1'){
+        $start=microtime();
+        $numder01=$_POST['numder01'];
+        $gen=3;
+        $i=2;
+        echo "<p class='t4'>尋找到第1個質數:2</p>";
+        while($i<=$numder01){
+            if(chk_prime($gen)){
+                echo "<p class='t4'>尋找到第".$i."個質數:".$gen."</p>";
+                $i++;
+                $gen+=2;
+            }else{$gen+=2;}}
+        $end=microtime();
+        echo "<p class='t3'>所需時間:".(($end-$start)*1000)."ms</p>";}
+    function chk_prime($intnumber){
+        $chkRange=$intnumber;
+        for($i=2;$i<$chkRange;$i=$i+1){
+            if($intnumber%$i==00){
+                return false;}}
+        return true;}
     ?>
 </body>
-
 </html>
--- homework/index.php
+++ homework/index.php
@@ -5,12 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php互動式程式設計作業</title>
+    
 </head>
 <body>
-    <p><a href="./ex1.php" target="main">題目一：亂數陣列排序</a></p>
-    <p><a href="./ex2.php" target="main">題目二：阿拉伯數字轉成國字</a></p>
-    <p><a href="./ex3.php" target="main">題目三：尋找質數程式</a></p>
-
+    <div style="background-color: rgb(1, 225, 255);"><h1>php互動式程式設計作業</h1></div>
+    <ul>
+    <p><a href="./ex1.php" target="main"><li>題目一：亂數陣列排序</a></li></p>
+    <p><a href="./ex2.php" target="main"><li>題目二：阿拉伯數字轉成國字</li></a></p>
+    <p><a href="./ex3.php" target="main"><li>題目三：尋找質數程式</li></a></p>
+    </ul>
     <iframe id="main" name="main" src="main.html" frameborder="1" width="1024" height="768" border="1"></iframe>
 
 </body>
